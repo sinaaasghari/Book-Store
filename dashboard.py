8633fc92043b18ee5470ffab72873055e5595ca6
Mohammad <mohammadnasimi6803@gmail.com>
2023-09-29 18:51:51 +0330

    Analytical charts question 1

---
+++ dashboard.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import mysql.connector
+import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
+import random
+import plotly.express as px
+
+conn = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='13771377Mnn@',
+    database='book_store'
+)
+
+cursor = conn.cursor()
+#color
+color =[
+            "#4477dd",
+            "#00a67e",
+            "#8ffe09",
+            "#c90b42",
+            "#ffaa0088"
+            ]
+
+
+st.title('BOOK STORE')
+
+tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+
+with tab1:
+    col1, col2 = st.columns([1, 3])
+
+    with col1:
+        type=st.radio(
+            "Analytical charts",
+            key="Analytical charts",
+            options=["count tag book", "hidden", "collapsed"],
+        )
+    with col2:
+        # First part: Analytical charts
+        if type == "count tag book":
+            # plot question 1
+            st.header('count tag book')
+            number = st.number_input('Choose number', step=1,value=20,min_value=5)
+            cursor.execute(f"select name,count(*) as count_book from group_category \
+                        inner join category c on group_category.category_id = c.id\
+                                group by name order by count_book desc limit {number}")
+            result = cursor.fetchall()
+            df = pd.DataFrame(
+                    result,
+                        columns=("name","count_book"))
+            ###
+            st.title('bar chart')
+            chart = alt.Chart(df).mark_bar().encode(
+                x=alt.X('name', title="دسته بندی ها"),
+                y=alt.Y('count_book', title="تعداد کتاب ها"),
+                color=alt.ColorValue(random.choice(color))
+            ).properties(
+                width=500,
+                height=300
+            )
+
+            st.altair_chart(chart, use_container_width=True)
+            ###
+            st.title('Pie chart')
+            fig = px.pie(df, values='count_book', names='name')
+
+            st.plotly_chart(fig, use_container_width=True)
